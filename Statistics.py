0d53fa4f8185df78015007f866bd6454cc80fbea
Aziza <rakhmonkulova.aziza@gmail.com>
2023-08-10 23:43:11 +0500

    update

--- Statistics.py
+++ Statistics.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 st.set_page_config(page_title = "This is a Multipage WebApp")
-st.title("This is the Home Page Geeks.")
+st.title("Stat results")
 # stm.sidebar.success("Select Any Page from here")
 
 # --------------------------------------------------------------------
